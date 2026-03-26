204e8e5bceb1a9d82c081f42ff28212033c9ae61
Adam Shimali <adam.shimali@dsit.gov.uk>
2026-03-26 08:57:43 +0000

    directory reorganisation

--- scripts/config.py
+++ scripts/config.py
@@ -7,8 +7,7 @@ DATA_GOV_URL = "https://data.gov.uk"
 COLLECTION_URL = DATA_GOV_URL + "/collections"
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = BASE_DIR / "data"
-COLLECTIONS_CSV = DATA_DIR / "collections" / "collection-urls.csv"
-RESULTS_DIR = DATA_DIR / "results"
+COLLECTIONS_CSV = BASE_DIR / "testing" / "collections" / "collection-urls.csv"
+RESULTS_DIR = BASE_DIR / "testing" / "results"
 REPO_URL = "https://github.com/alphagov/datagovuk_find.git"
 COLLECTIONS_SUBDIR = "app/content/collections"
